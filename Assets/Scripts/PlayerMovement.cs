ef61ff0df1b871b26e2e83534e9f88634cca5285
kristina-99 <kristina_atan@abv.bg>
2026-04-14 14:37:43 +0300

    * created a Run method to move the player on the x axis * the player speed is controlled by the moveSpeed variable

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,27 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    [SerializeField] float moveSpeed = 10f;
+
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();    
     }
 
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidBody.linearVelocity.y);
+        myRigidBody.linearVelocity = playerVelocity;
     }
 }
